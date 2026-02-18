365c8fc4dd2fa656d5c2f4b3942c36d920a73c40
Nilo Arthur <niloarthurpr@gmail.com>
2026-02-18 20:40:20 -0300

    update scroll
    Added event listeners for mouse enter and leave on the scroll container.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let total= 0;
+let total = 0;
 
-function cart(){
+function cart() {
 
     total++;
 
     document.getElementById('all').innerText = total;
 }
-function restart(){
-    total=0
+function restart() {
+    total = 0
 
-    document.getElementById('all').innerText= total;
+    document.getElementById('all').innerText = total;
 }
+
+
+const container = document.getElementById('myScroll');
+
+    container.addEventListener('mouseenter', () => {
+        container.focus();
+    });
+
+    container.addEventListener('mouseleave', () => {
+    container.blur();
+});
